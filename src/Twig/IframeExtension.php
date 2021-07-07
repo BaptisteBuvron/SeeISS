25c269f8c6481545eb0ce6995795fffecf0a6170
Baptiste Buvron <baptiste.buvron@gmail.com>
2021-07-07 13:57:14 +0200

    Create Iframe filter

---
+++ src/Twig/IframeExtension.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace App\Twig;
+
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class IframeExtension extends AbstractExtension
+{
+
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('iframe', [$this, 'createIframe']),
+        ];
+    }
+
+    public function createIframe($url): string
+    {
+        $parsedUrl = parse_url($url);
+        # extract query string
+        parse_str(@$parsedUrl['query'], $queryString);
+        $youtubeId = @$queryString['v'] ?? substr(@$parsedUrl['path'], 1);
+        return "https://youtube.com/embed/{$youtubeId}";
+    }
+}
+
